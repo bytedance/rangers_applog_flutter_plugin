cff59552b7f59fa0194de770ac4dddd8311c1a6e
朱元清 <zhuyuanqing@bytedance.com>
2021-06-02 15:37:44 +0800

    feat: ios: use instance method

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -15,21 +15,21 @@
     /* 初始化开始。请参考iOS原生接入文档。
      * Init SDK. Please refer to iOS integrate doc on the official website.
      */
-    BDAutoTrackConfig *config =[BDAutoTrackConfig configWithAppID:@"your appID"];// 如不清楚请联系专属客户成功经理
-          
-    config.serviceVendor = BDAutoTrackServiceVendorCN;
-
-    config.appName = @"your appName"; // 与您申请APPID时的app_name一致
-    config.channel = @"App Store"; // iOS一般默认App Store
-    
-    config.showDebugLog = NO; // 是否在控制台输出日志，仅调试使用。release版本请设置为 NO
-    config.logger = ^(NSString * _Nullable log) {
-        NSLog(@"%@",log);
-    };
-    config.logNeedEncrypt = YES; // 是否加密日志，默认加密。release版本请设置为 YES
-
-    [BDAutoTrack startTrackWithConfig:config];
-    
+//    BDAutoTrackConfig *config =[BDAutoTrackConfig configWithAppID:@"your appID"];// 如不清楚请联系专属客户成功经理
+//
+//    config.serviceVendor = BDAutoTrackServiceVendorCN;
+//
+//    config.appName = @"your appName"; // 与您申请APPID时的app_name一致
+//    config.channel = @"App Store"; // iOS一般默认App Store
+//
+//    config.showDebugLog = NO; // 是否在控制台输出日志，仅调试使用。release版本请设置为 NO
+//    config.logger = ^(NSString * _Nullable log) {
+//        NSLog(@"%@",log);
+//    };
+//    config.logNeedEncrypt = YES; // 是否加密日志，默认加密。release版本请设置为 YES
+//
+//    [BDAutoTrack startTrackWithConfig:config];
+//
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
