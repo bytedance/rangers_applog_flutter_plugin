aa46123e10ed8c767afdb78e9a87cd9fe8096e0f
朱元清 <zhuyuanqing@bytedance.com>
2021-06-02 14:50:25 +0800

    ios: bridge init method

--- example/ios/Runner/AppDelegate.m
+++ example/ios/Runner/AppDelegate.m
@@ -8,12 +8,15 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
-   
-    // ！注意：如果在此处初始化了，就不要调用插件的initRangersAppLog进行初始化，以免重复初始化出现问题！
-    /* 初始化开始 */
+    
+    /* ！注意：如果在此处初始化了，就不要调用插件的initRangersAppLog进行初始化，以免重复初始化出现问题！
+     * ! Note: Hereby we init SDK using native Objective-C. You might use `initRangersAppLog` to do the same (the native way is better in performance, though). But don't do both.
+     */
+    /* 初始化开始。请参考iOS原生接入文档。
+     * Init SDK. Please refer to iOS integrate doc on the official website.
+     */
     BDAutoTrackConfig *config =[BDAutoTrackConfig configWithAppID:@"your appID"];// 如不清楚请联系专属客户成功经理
           
-    /* 数据上报*/
     config.serviceVendor = BDAutoTrackServiceVendorCN;
 
     config.appName = @"your appName"; // 与您申请APPID时的app_name一致
@@ -26,7 +29,6 @@
     config.logNeedEncrypt = YES; // 是否加密日志，默认加密。release版本请设置为 YES
 
     [BDAutoTrack startTrackWithConfig:config];
-    /* 初始化结束 */
     
     return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
